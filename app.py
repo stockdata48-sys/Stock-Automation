4566d428d572ae701f11232e4bbd1038e3735b69
stockdata48-sys <stockdata48@gmail.com>
2025-09-05 16:11:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ import final_automation
 app = FastAPI()
 API_KEY = "150697"
 
-@app.post("/run-job")
+@app.post("/run-job", methods=["GET", "POST"])
 def run_job(api_key: str = Query(...), file_id: str = "1WuTVktuDnJh3UECy4EzeaAlH4Bm6pAY7"):
     if api_key != API_KEY:
         return {"status": "error", "message": "Unauthorized"}
